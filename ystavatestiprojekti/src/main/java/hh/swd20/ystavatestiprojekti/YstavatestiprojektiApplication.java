949635fcf95e86702f7fca26c51f9a6f26aed8af
ehsan16 <ehsan_haidari_11@hotmail.com>
2019-01-30 10:35:51 +0200

    first commit

---
+++ ystavatestiprojekti/src/main/java/hh/swd20/ystavatestiprojekti/YstavatestiprojektiApplication.java
@@ -0,0 +1,14 @@
+package hh.swd20.ystavatestiprojekti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YstavatestiprojektiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YstavatestiprojektiApplication.class, args);
+	}
+
+}
+
